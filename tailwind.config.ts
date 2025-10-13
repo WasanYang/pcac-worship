3db7f8c47f2ba490e7ceb0c9d2b8a66ca2d72a21
Es Yaj <esxy26@gmail.com>
2025-10-13 07:50:35 +0000

    ปรัให้ใช้ font noto sans thai

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -17,8 +17,8 @@ export default {
     },
     extend: {
       fontFamily: {
-        body: ['PT Sans', 'sans-serif'],
-        headline: ['PT Sans', 'sans-serif'],
+        body: ['Noto Sans Thai', 'sans-serif'],
+        headline: ['Noto Sans Thai', 'sans-serif'],
         code: ['monospace'],
       },
       colors: {
