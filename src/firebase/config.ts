acebce21955553194b3cdcdfa70f6be710aff5f7
Es Yaj <esxy26@gmail.com>
2025-10-13 10:31:14 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-7303019413-59816",
+  "appId": "1:47156830007:web:1519ba27b180bf12f384b2",
+  "apiKey": "AIzaSyA-SGMf1xM_eZYe3pPinSmJgJjbfZ9IYPY",
+  "authDomain": "studio-7303019413-59816.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "47156830007"
+};
