3bc20321f957ad18f9fdd693ac77b206f858a246
Es Yaj <esxy26@gmail.com>
2025-10-13 08:24:25 +0000

    ทำให้เป็น PWA ให้หน่อย

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,6 +5,7 @@ import "./globals.css";
 export const metadata: Metadata = {
   title: "Prasiri Worship Manager",
   description: "Manage your worship services with ease.",
+  manifest: "/manifest.json",
 };
 
 export default function RootLayout({
@@ -15,6 +16,8 @@ export default function RootLayout({
   return (
     <html lang="en" suppressHydrationWarning>
       <head>
+        <meta name="theme-color" content="#1DB954" />
+        <link rel="apple-touch-icon" href="/icons/icon-192x192.png"></link>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link
